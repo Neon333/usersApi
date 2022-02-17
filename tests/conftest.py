66ce7d818c3e484db96c284ba9eca937d6d02477
Neon333 <rixibike@gmail.com>
2022-02-17 19:56:39 +0300

    removed unused argumemnt from fixture

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,11 +3,7 @@ import asyncio
 
 
 @pytest.yield_fixture(scope="session")
-def event_loop(request):
-    """Create an instance of the default event loop for each test case."""
+def event_loop():
     loop = asyncio.get_event_loop_policy().new_event_loop()
     yield loop
     loop.close()
-
-
-asyncio_mode = 'auto'
